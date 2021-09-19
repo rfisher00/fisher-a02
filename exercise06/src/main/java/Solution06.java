105d9573b87a39f291c7e77820e6a7dc3c3d0e62
ryanf <ryanfisher00@outlook.com>
2021-09-19 15:31:31 -0400

    first commit

---
+++ exercise06/src/main/java/Solution06.java
@@ -0,0 +1,30 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution 06
+ *  Copyright 2021 Ryan Fisher
+ */
+
+import java.util.*;
+
+public class Solution06 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("What is your current age? ");
+        String ageStr = sc.nextLine();
+        int age = Integer.parseInt(ageStr);
+
+        System.out.print("At what age would you like to retire? ");
+        String retireStr = sc.nextLine();
+        int retireAge = Integer.parseInt(retireStr);
+
+        int ageDiff = retireAge - age;
+
+        System.out.printf("You have %d years left until you can retire%n", ageDiff);
+
+        Date date = new Date();
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        int currYear = calendar.get(Calendar.YEAR);
+
+        System.out.printf("It's %d, so you can retire in %d", currYear, currYear + ageDiff);
+    }
+}
